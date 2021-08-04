f4a1deaf9725205f9a647df120631ca714224ced
henriquenassary <henriquenassary@gmail.com>
2021-08-04 14:24:35 -0300

    Commit Inicial

---
+++ src/main/java/com/nassary/docone_club/DoconeClubApplication.java
@@ -0,0 +1,13 @@
+package com.nassary.docone_club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoconeClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoconeClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nassary/docone_club/DoconeClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nassary.docone_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoconeClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
